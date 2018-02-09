533a36ad42ae0473fcd6e5e653b0cfe1eff99281
Marek Pribyl <Marek.Pribyl@4finance.com>
2018-02-09 17:51:59 +0100

    WIP

--- public/script.js
+++ public/script.js
@@ -1,1 +1,33 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [
+            { id: 1, title: 'one', price: 19.90 },
+            { id: 2, title: 'two', price: 49.90 }
+        ],
+        cart: []
+    },
+    methods: {
+        addItemToCart: function(index) {
+            var item = this.items[index];
+            this.total += item.price;
+            var found = false;
+            for (var i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].id === item.id) {
+                    this.cart[i].qty++;
+                    found = true;
+                }
+            }
+
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    price: item.price,
+                    qty: 1
+                });
+            }
+        }
+    }
+});
